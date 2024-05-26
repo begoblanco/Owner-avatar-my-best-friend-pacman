b553a6f163d63303511f107b0e64f218bc17cdee
Bego Blanco <begoblanfer@gmail.com>
2024-05-26 14:20:23 +0200

    eat fantasmico implemented

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+let fantasmico;
+let pacman;
 
-document.addEventListener('DOMContentLoaded', (event) => {
-   
-    const image1 = document.getElementById('/public/assets/media/fantasmito.png');
-    const image2 = document.getElementById('/public/assets/media/pacman-icon.png');
-  
-    
-    image1.addEventListener('click', () => {
-      // Hide the first image
-      image1.style.display = 'none';
-      // Show the second image
-      image2.style.display = 'block';
-    });
-  });
+function loaded() {
+  fantasmico = document.getElementById("fantasmito-image"); //seleccionar elemento
+  fantasmico.addEventListener("click", eatFantasmico); //registrar evento al elemento
+  pacman = document.getElementById("pacman-main-image"); 
+  // fantasmico.addEventListener("click", hidePacman); 
+
+}
+
+document.addEventListener("DOMContentLoaded", loaded); //espera a que el dom termine de cargar
+
+function eatFantasmico() {
+  fantasmico.style.setProperty("visibility", "hidden");
+  fantasmico.src="./public/assets/media/pacman-icon.png"; //cambia fantasmico por pacman
+  fantasmico.style.setProperty("visibility", "visible"); //ahora se ve la imagen de pacman en su lugar
+  pacman.style.setProperty("visibility", "hidden"); //esconde el pacman original
+}
+
+// function hidePacman(){
+//   pacman.style.setProperty("visibility", "hidden");
+// }
+
+
